ce83ee33b70b9965055f4a8b579ac07138e08801
Chris Morris <cmorris@Chriss-iMac-2.local>
2022-02-16 16:57:04 +0000

    Final commit before leaving office Wednesday

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,7 +2,10 @@ class StocksController < ApplicationController
 
   def search
     @stock = Stock.new_lookup(params[:stock])
-
-    render 'users/my_portfolio'
+    if @stock
+      respond_to do |format|
+        format.js {render partial: 'users/result'}
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
+  resources :user_stocks
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "welcome#index"
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
